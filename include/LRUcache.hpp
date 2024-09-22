93ba35060fa17c1bd100d58db9161be2225920bb
egor7531 <egorkokonin557@gmail.com>
2024-09-22 22:07:41 +0300

    fixed the negligence

--- include/LRUcache.hpp
+++ include/LRUcache.hpp
@@ -1,46 +1,49 @@
-#ifndef LRUCACHE_HPP
-#define LRUCACHE_HPP
+#pragma once
 
 #include <iterator>
 #include <list>
 #include <unordered_map>
 
-template <typename T, typename KeyT = int>
-struct lru_cache_t
+namespace cache
 {
-    size_t size;
-    std::list<std::pair<KeyT, T>> cache;
-
-    using ListIt = typename std::list<std::pair<KeyT, T>>::iterator;
-    std::unordered_map<KeyT, ListIt> hash;
-    
-    lru_cache_t(size_t sz): size(sz) {}
-
-    bool full() const { return size == cache.size(); }
-
-    template <typename F> 
-    bool lookup_update(KeyT key, F slow_get_page)
+    template <typename T, typename KeyT = int>
+    class lru_cache_t
     {
-        auto hit = hash.find(key);
-        if(hit == hash.end())
-        {
-            if(full())
+            size_t size;
+            std::list<std::pair<KeyT, T>> cache;
+
+            using ListIt = typename std::list<std::pair<KeyT, T>>::iterator;
+            std::unordered_map<KeyT, ListIt> hash;
+
+        public:
+            
+            lru_cache_t(size_t sz): size(sz) {}
+            
+            bool full() const { return size == cache.size(); }
+
+            template <typename F> 
+            bool lookup_update(KeyT key, F slow_get_page)
             {
-                hash.erase(cache.back().first);
-                cache.pop_back();
+                auto hit = hash.find(key);
+
+                if(hit == hash.end())
+                {
+                    if(full())
+                    {
+                        hash.erase(cache.back().first);
+                        cache.pop_back();
+                    }
+
+                    cache.emplace_front(key, slow_get_page(key));
+                    hash.emplace(key, cache.begin());
+                    return false;
+                }
+
+                auto eltit = hit->second;
+                if(eltit != cache.begin())
+                    cache.splice(cache.begin(), cache, eltit, std::next(eltit));
+                
+                return true;
             }
-
-            cache.emplace_front(key, slow_get_page(key));
-            hash.emplace(key, cache.begin());
-            return false;
-        }
-
-        auto eltit = hit->second;
-        if(eltit != cache.begin())
-            cache.splice(cache.begin(), cache, eltit, std::next(eltit));
-        
-        return true;
-    }
-};
-
-#endif //LRUCACHE_HPP
+    };
+}
