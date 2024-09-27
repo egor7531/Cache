c826ce2f9e4c3bf729908cd463fd8207068aebe6
egor7531 <egorkokonin557@gmail.com>
2024-09-27 13:02:45 +0300

    added a cache comparison

---
+++ tests/CompareCaches.py
@@ -0,0 +1,19 @@
+
+numTests = int(open("./tests/NumberTests.txt", 'r').read())
+
+file = open("tests/testsFiles/Answer/RatioCaches.txt", 'w')
+file.write("Perfect/LRU" + "\t" + "Perfect/LFU" + "\t" + "LFU/LRU" + "\n")
+
+for i in range(1, numTests + 1):
+
+    ansLRU      = int(open("tests/testsFiles/Answer/LRU/"       + f'{i :02}' + ".txt", 'r').read())
+    ansLFU      = int(open("tests/testsFiles/Answer/LFU/"       + f'{i :02}' + ".txt", 'r').read())
+    ansPerfect  = int(open("tests/testsFiles/Answer/PERFECT/"   + f'{i :02}' + ".txt", 'r').read())
+
+    ratio31 = round((float)(ansPerfect) / (float)(ansLRU), 2)
+    ratio32 = round((float)(ansPerfect) / (float)(ansLFU), 2)
+    ratio21 = round((float)(ansLFU)     / (float)(ansLRU), 2)
+
+    file.write(f'{ratio31:<10}' + "\t" + f'{ratio32:<10}' + "\t" + f'{ratio21:<10}' + "\n")
+
+file.close()
